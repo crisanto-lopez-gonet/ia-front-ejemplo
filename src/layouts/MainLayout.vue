f70ab8867642b870edef54eb0e4c79bf8da9ebe1
Diego Ceron <diego.ceron@gonet.us>
2021-10-05 14:13:59 -0500

    Add quasar

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,85 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated class="clase">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Intelligence Accounting
+        </q-toolbar-title>
+
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Palace Resorts',
+    caption: 'palace.resorts',
+    icon: 'school',
+    link: 'https://www.palaceresorts.com/es'
+  }
+];
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
+
+<style lang="sass">
+.clase
+  background-color: $pr-title
+</style>
